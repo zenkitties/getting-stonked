21b8b79fe5adc257580ad016c62261d738f6b1fb
Nick Myers <zenfootphilosopher@gmail.com>
2023-03-20 10:59:34 -0700

    Renamed index.jsx in app component

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../Navbar' 
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <h1>Welcome to Getting Stonked!</h1>
+    </div>
+  );
+}
+
+export default App;
