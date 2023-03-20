b0d00bfd3df6783208d405fd0bed15af225f5840
Nick Myers <zenfootphilosopher@gmail.com>
2023-03-20 10:56:52 -0700

    Rename file extension. Add Navbar component.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,23 @@
+import Logo from '../Logo'
+import NavItem from '../NavItem'
+
+import NAVITEMS from '../../navItems.json'
+
 import './Navbar.scss'
 
 const Navbar = () => {
+    const getNavItems = (navItem) => {
+        const {name, route} = navItem;
+
+        return <NavItem name={name} />
+    }
+
     return (
         <div className="navbar-container">
-            
+            <Logo />
+            <ul className="nav-items">
+                {NAVITEMS.map(getNavItems)}
+            </ul>
         </div>
     )
 }
